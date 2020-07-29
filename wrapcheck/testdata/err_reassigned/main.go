aa1d658d8deae58a174ef18e33476e157a7b847e
Tom Arrell <tom.arrell@gmail.com>
2020-07-29 12:53:10 +0200

    Add test case for err reassignment

---
+++ wrapcheck/testdata/err_reassigned/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+)
+
+func main() {
+	do()
+}
+
+func do() error {
+	_, err := json.Marshal(struct{}{})
+	if err != nil {
+		return fmt.Errorf("failed to marshal struct", err)
+	}
+
+	// This should not error, as it's a call into a local function which returns
+	// an error
+	err = returnsError()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func returnsError() error {
+	return errors.New("failed")
+}
