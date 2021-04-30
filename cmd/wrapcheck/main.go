0a47cbdcda80c4567320ecb75e6cce49fd1c9b00
Tom Arrell <tom.arrell@gmail.com>
2021-04-30 15:32:29 +0200

    Add configuration file support

--- cmd/wrapcheck/main.go
+++ cmd/wrapcheck/main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"log"
+
+	"github.com/spf13/viper"
 	"github.com/tomarrell/wrapcheck/wrapcheck"
 	"golang.org/x/tools/go/analysis/singlechecker"
 )
 
 func main() {
-	singlechecker.Main(wrapcheck.Analyzer)
+	viper.SetConfigName(".wrapcheck")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("$HOME/.wrapcheck")
+	viper.AddConfigPath(".")
+
+	viper.SetDefault("ignoreSigs", wrapcheck.DefaultIgnoreSigs)
+
+	// Read in config, ignore if the file isn't found and use defaults.
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			log.Fatalf("failed to parse config: %v", err)
+		}
+	}
+
+	var cfg wrapcheck.WrapcheckConfig
+	if err := viper.Unmarshal(&cfg); err != nil {
+		log.Fatalf("failed to unmarshal config: %v", err)
+	}
+
+	singlechecker.Main(wrapcheck.NewAnalyzer(cfg))
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,14 @@ module github.com/tomarrell/wrapcheck
 go 1.14
 
 require (
+	github.com/armon/consul-api v0.0.0-20180202201655-eb2c6b5be1b6 // indirect
+	github.com/coreos/go-etcd v2.0.0+incompatible // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
+	github.com/spf13/viper v1.7.1 // indirect
 	github.com/stretchr/testify v1.6.1
+	github.com/ugorji/go/codec v0.0.0-20181204163529-d75b2dcb6bc8 // indirect
+	github.com/xordataexchange/crypt v0.0.3-0.20170626215501-b2862e3d0a77 // indirect
 	golang.org/x/tools v0.0.0-20200804011535-6c149bb5ef0d
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
